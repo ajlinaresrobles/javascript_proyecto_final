6935d136cc4f6b0e7b29a44f857b5f198aa9f6d5
alinares <alejandrolinares775@gmail.com>
2023-03-01 06:47:30 -0300

    tercera entrega

--- proyecto_final.js
+++ proyecto_final.js
@@ -1,190 +1,165 @@
-// convertidor de unidades
 
-// creación del objeto
+
+let mensaje = document.createElement("p");
+
+
+
+function conv_moneda() {
+
+    let cantidad = document.getElementById("input_cantidad");
+    let moneda_desde = document.getElementById("moneda_desde");
+    let moneda_hasta = document.getElementById("moneda_hasta");
+    let output = document.getElementById("output");
+
+    function calculo (razon) {
+        
+        let resultado = cantidad.value * razon;
+        mensaje.innerHTML =`Recibirás ${resultado.toFixed(2)} ${moneda_hasta.value}`;
+    };
+   
+    if (moneda_desde.value == "Dolares Americanos ($)") {
+        if (moneda_hasta.value == "Dolares Americanos ($)") {calculo(1)}
+        else if(moneda_hasta.value == "Pesos Chilenos (CLP)") {calculo(810)}
+        else if(moneda_hasta.value == "Euros (€)") {calculo(0.915254)}
+        else if(moneda_hasta.value == "Pesos Argentinos (ARS)") {calculo(337.5)};
+    }
+    if (moneda_desde.value == "Pesos Chilenos (CLP)") {
+        if (moneda_hasta.value == "Dolares Americanos ($)") {calculo(0.001205)}
+        else if(moneda_hasta.value == "Pesos Chilenos (CLP)") {calculo(1)}
+        else if(moneda_hasta.value == "Euros (€)") {calculo(0.00113)}
+        else if(moneda_hasta.value == "Pesos Argentinos (ARS)") {calculo(0.41667)};
+    }
+    if (moneda_desde.value == "Euros (€)") {
+        if (moneda_hasta.value == "Dolares Americanos ($)") {calculo(1.036145)}
+        else if(moneda_hasta.value == "Pesos Chilenos (CLP)") {calculo(860)}
+        else if(moneda_hasta.value == "Euros (€)") {calculo(1)}
+        else if(moneda_hasta.value == "Pesos Argentinos (ARS)") {calculo(358.3333)};
+    }
+    if (moneda_desde.value == "Pesos Argentinos (ARS)") {
+        if (moneda_hasta.value == "Dolares Americanos ($)") {calculo(0.00253)}
+        else if(moneda_hasta.value == "Pesos Chilenos (CLP)") {calculo(2.1)}
+        else if(moneda_hasta.value == "Euros (€)") {calculo(0.002373)}
+        else if(moneda_hasta.value == "Pesos Argentinos (ARS)") {calculo(1)};
+    }
+
+    output.append(mensaje);
+}
+
+let btn_conversion = document.getElementById("btn_conversion");
+
+btn_conversion.addEventListener("click", conv_moneda);
+
+
+
+
+class Usuario{
+
+        constructor(email_usuario, pass_usuario, nombre_usuario, apellido_usuario, edad_usuario){
     
-class Conversion{
-
-    constructor(propiedad, numero, unidad_elegida){
-
-        this.propiedad = propiedad;
-        this.numero = numero;
-        this.unidad_elegida = unidad_elegida;
+            this.email_usuario = email_usuario;
+            this.pass_usuario = pass_usuario;
+            this.nombre_usuario = nombre_usuario;
+            this.apellido_usuario = apellido_usuario;
+            this.edad_usuario = edad_usuario;
+        }
     }
 
-}
-// declaración de array vacía
-let lista_conversion = []; 
+    let lista_usuarios = [];
 
-// Se ingresa la propiedad
+    // Recupero arreglo de objetos almacenados
+    let recuperando_usuarios = localStorage.getItem("arreglo_JSON");
+    lista_usuarios = JSON.parse(recuperando_usuarios);
 
-let propiedad = prompt("Cuál propiedad desea convertir?\n - longitud \n - masa \n - volumen");
 
-                        // comienza el ciclo while
-
-while (propiedad != "salir") {
+function alta_usuario() {
     
-// se ingresa la cantidad
+    let email_usuario = document.getElementById("email_usuario");
+    let pass_usuario = document.getElementById("pass_usuario");
+    let nombre_usuario = document.getElementById("nombre_usuario");
+    let apellido_usuario = document.getElementById("apellido_usuario");
+    let edad_usuario = document.getElementById("edad_usuario");
 
-let numero = prompt("Ingrese la cantidad a convertir");
-
-
-if (isNaN(numero)) {
-    alert("La cantidad debe ser numérica");
-    break
-}
-else{
-                    // FUNCIONES
-
-function calc_conv_longitud(constante1, constante2, constante3, constante4, unidad ) {
-    let result_centimetro = numero * constante1;
-    let result_metro = numero * constante2;
-    let result_pie = numero * constante3;
-    let result_pulgada = numero * constante4;
-    alert("el resultado es el siguiente: \n" + 
-        numero + " " + unidad + " equivalen a " + result_centimetro + " centímetros \n" +
-        numero + " " + unidad + " equivalen a " + result_metro + " metros \n" +
-        numero + " " + unidad + " equivalen a " + result_pie + " pies \n" + 
-        numero + " " + unidad + " equivalen a " + result_pulgada + " pulgadas \n");
-
-}
-function calc_conv_masa(constante1, constante2, constante3, constante4, unidad) {
-    let result_gramo = numero * constante1;
-    let result_kilogramo = numero * constante2;
-    let result_libra = numero * constante3;
-    let result_tonelada = numero * constante4;
-    alert("el resultado es el siguiente: \n" + 
-        numero + " " + unidad + " equivalen a " + result_gramo + " gramos \n" +
-        numero + " " + unidad + " equivalen a " + result_kilogramo + " kilogramos \n" +
-        numero + " " + unidad + " equivalen a " + result_libra + " libras \n" + 
-        numero + " " + unidad + " equivalen a " + result_tonelada + " toneladas \n");
-}
-
-function calc_conv_volumen(constante1, constante2, constante3, constante4, unidad) {
-    let result_litro = numero * constante1;
-    let result_galon = numero * constante2;
-    let result_onza = numero * constante3;
-    let result_metros_cubicos = numero * constante4;
-    alert("el resultado es el siguiente: \n" + 
-        numero + " " + unidad + " equivalen a " + result_litro + " litros \n" +
-        numero + " " + unidad + " equivalen a " + result_galon + " galones \n" +
-        numero + " " + unidad + " equivalen a " + result_onza + " onzas \n" + 
-        numero + " " + unidad + " equivalen a " + result_metros_cubicos + " metros cúbicos \n");
-}
-
-
-                    // CONDICIONES
-
-if (propiedad == "longitud" || propiedad == "LONGITUD") {
-    let unidad_elegida = prompt("ingrese la unidad a convertir \n - centímetros \n - metros \n - piés \n - pulgadas");
-
-    if (unidad_elegida == "metros" || unidad_elegida == "METROS") {
-        calc_conv_longitud(100, 1, 3.28, 39.37, unidad_elegida);   
+    if (email_usuario.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe llenar todos los campos para poder registrarse',
+          })
+    } 
+    else if (pass_usuario.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe llenar todos los campos para poder registrarse',
+          })
     }
-
-    else if (unidad_elegida == "centimetros" || unidad_elegida == "CENTIMETROS" || unidad_elegida == "centímetros" || unidad_elegida == "CENTÍMETROS") {
-        calc_conv_longitud(1, 0.01, 0.03, 0.39, unidad_elegida);
+    else if (nombre_usuario.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe llenar todos los campos para poder registrarse',
+          })
     }
-
-    else if (unidad_elegida == "pies" || unidad_elegida == "piés" || unidad_elegida == "PIES" || unidad_elegida == "PIÉS") {
-        calc_conv_longitud(30.48, 0.3048, 1, 12, unidad_elegida);
+    else if (apellido_usuario.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe llenar todos los campos para poder registrarse',
+          })
     }
-
-    else if (unidad_elegida == "pulgadas" || unidad_elegida == "PULGADAS") {
-        calc_conv_longitud(2.54, 0.0254, 0.8333, 1, unidad_elegida);
+    else if (edad_usuario.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe llenar todos los campos para poder registrarse',
+          })
     }
     else {
-        alert("No ingresó ninguna de las unidades solicitadas");
-        break
+        let nuevo_usuario = new Usuario(email_usuario.value, pass_usuario.value, nombre_usuario.value, apellido_usuario.value, edad_usuario.value);
+
+    lista_usuarios.push(nuevo_usuario);
+
+    let arreglo_JSON = JSON.stringify(lista_usuarios);
+
+    localStorage.setItem("arreglo_JSON", arreglo_JSON);
+    Toastify({
+
+        text: "Registro Exitoso",
+        
+        duration: 3000,
+        gravity: "bottom"
+        }).showToast();
     }
-
-    let nueva_conversion = new Conversion(propiedad, numero, unidad_elegida);
-
-    lista_conversion.push(nueva_conversion);
-}
-
-else if (propiedad == "masa" || propiedad == "MASA") {
     
-    unidad_elegida = prompt("ingrese la unidad a convertir \n - gramos \n - kilogramos \n - libras \n - toneladas");
-    if (unidad_elegida == "gramos") {
-        calc_conv_masa(1, 0.001, 0.0022, 0.000001, unidad_elegida);
-    }
-
-    else if (unidad_elegida == "kilogramos" || unidad_elegida == "KILOGRAMOS") {
-        calc_conv_masa(1000, 1, 2.2046, 0.001, unidad_elegida);
-    }
-    else if (unidad_elegida == "libras" || unidad_elegida == "LIBRAS") {
-        calc_conv_masa(453.59, 0.45359, 1, 0.00045359, unidad_elegida);
-    }
-
-    else if (unidad_elegida == "toneladas" || unidad_elegida == "TONELADAS") {
-        calc_conv_masa(1000000, 1000, 2204.6, 1, unidad_elegida);
-    }
-    else {
-        alert("No ingresó ninguna de las unidades solicitadas");
-        break
-    }
-    nueva_conversion = new Conversion(propiedad, numero, unidad_elegida);
-
-    lista_conversion.push(nueva_conversion);
-}
-
-else if (propiedad == "volumen" || propiedad == "VOLUMEN") {
-    
-    unidad_elegida = prompt("ingrese la unidad a convertir \n - litros \n - galones \n - onzas \n - metros cúbicos");
-    if (unidad_elegida == "litros" || unidad_elegida == "LITROS") {
-        calc_conv_volumen(1, 0.2199, 33.81, 0.001, unidad_elegida);
-    }
-
-    else if (unidad_elegida == "galones" || unidad_elegida == "GALONES") {
-        calc_conv_volumen(4.546, 1, 153.72, 0.004546, unidad_elegida);
-    }
-    else if (unidad_elegida == "onzas" || unidad_elegida == "ONZAS") {
-        calc_conv_volumen(0.295, 0.00625, 1, 0.0000295, unidad_elegida);
-    }
-
-    else if (unidad_elegida == "metros cubicos" || unidad_elegida == "METROS CUBICOS" || unidad_elegida == "metros cúbicos" || unidad_elegida == "METROS CÚBICOS") {
-        calc_conv_volumen(1000, 219.96, 33814.02, 1, unidad_elegida);
-    }
-    else {
-        alert("No ingresó ninguna de las unidades solicitadas");
-        break
-    }
-    nueva_conversion = new Conversion(propiedad, numero, unidad_elegida);
-
-    lista_conversion.push(nueva_conversion);
-}
-else {
-    alert("No ingresó ninguna de las propiedades anteriormente solicitadas");
-    break
-}
-}
-
-// se ingresa nueva propiedad para evitar el bucle infinito
-
-propiedad = prompt("Cuál propiedad desea convertir?\n - longitud \n - masa \n - volumen \n o escriba salir para terminar");
-
 
 }
-                    // fin del ciclo while
 
+function login_usuario() {
+   
+   let email_login = document.getElementById("email_login");
+   let pass_login = document.getElementById("pass_login");
+   let recuperando_usuarios = localStorage.getItem("arreglo_JSON");
+   lista_usuarios = JSON.parse(recuperando_usuarios);
 
-                    // función lógica para el sort
+    for (let usuario of lista_usuarios) {
+        if (email_login.value == usuario.email_usuario && pass_login.value == usuario.pass_usuario) {
+            document.body.innerHTML = `<h2>bienvenido ${usuario.nombre_usuario}</h2>
+                                    <a href="index.html">volver</a>`
+                                    break
+        }
+        else{
+            document.body.innerHTML = `<h2>Usuario no encontrado ${email_login.value}</h2>
+                                    <a href="index.html">volver</a>`
+        }
+    }
 
-function ordenar(a, b) {
-     if (a.propiedad < b.propiedad) {
-       return -1 
-     } 
-     else if (a.propiedad > b.propiedad){
-       return 1 
-     }
-     else {
-        return 0
-     }
 }
 
 
-let lista_ordenada = lista_conversion.sort(ordenar);
+let btn_registro = document.getElementById("btn_registro");
 
-console.log(lista_ordenada);
+btn_registro.addEventListener("click", alta_usuario);
 
+let btn_login = document.getElementById("btn_login");
 
+btn_login.addEventListener("click", login_usuario);
